551b01e97e226bc9b6dc1ee0330dc182b3ad1e6a
Habtamu210 <beleyaha@gmail.com>
2023-07-14 04:58:32 -0700

    create car model and add validation

---
+++ app/models/car.rb
@@ -0,0 +1,10 @@
+class Car < ApplicationRecord
+  #validation
+  validates :name, presence: true, message: "Please enter car name"
+  validates :price, numericality: { greater_than: 0, message: "Please enter a price greater than 0" }
+  validates :model, presence: true, message: "Please enter car model"
+
+  # association
+  has_many :reservations
+  has_many_through :user
+end
