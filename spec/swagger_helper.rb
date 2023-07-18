7dee6c723c7d4739bcddb410f1d2c00189667b28
Enis <enis.mema@gmail.com>
2023-07-18 14:17:57 +0200

    Rubocop fix

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.configure do |config|
@@ -22,7 +20,6 @@ RSpec.configure do |config|
         version: 'v1'
       },
       paths: {},
-  
 
       servers: [
         {
